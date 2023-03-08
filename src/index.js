9b9fb8c914873147d640d9dd8bc50bc27dbdce32
Zac <121649297+zac-march@users.noreply.github.com>
2023-03-09 07:47:10 +1100

    Add styling and refactor style readability

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import { navbar } from "./navbar";
 import { loadPage } from "./loadpage";
 import "./style.css";
 
-const content = document.querySelector("#content");
 navbar.activeLink.toggleActive();
 loadPage(navbar.activeLink.element);
