e4defecd80f679df88e041a5a97d4d3b95d5ae7a
Zac <121649297+zac-march@users.noreply.github.com>
2023-03-09 12:56:29 +1100

    Fix logo image

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import { navbar } from "./navbar";
 import { loadPage } from "./loadpage";
+import Logo from "./images/logo.jpeg";
 import "./style.css";
+
+const logo = document.querySelector("#logo");
+logo.src = Logo;
 
 navbar.activeLink.toggleActive();
 loadPage(navbar.activeLink.element);
